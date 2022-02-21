a7b35a95dac7c95f626bd19b8d7e95254790a340
mcdatas <96379013+mcdatas@users.noreply.github.com>
2022-02-22 00:16:26 +0530

    Update readvideo.py

--- readvideo.py
+++ readvideo.py
@@ -11,21 +11,14 @@ while True:
     ret,frame = video.read()
     if ret == True:
         gray_video = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #gray_video = cv2.resize(gray_video, (500, 500))
 
         plate = plat_detector.detectMultiScale(gray_video,scaleFactor=1.2,minNeighbors=5,minSize=(25,25))
 
         for (x,y,w,h) in plate:
             cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0),2)
-            #frame[y:y+h,x:x+w] = cv2.blur(frame[y:y+h,x:x+w],ksize=(10,10))
-            #text1 = text.readtext(np.array(gray_video * 255).astype('uint8'), paragraph="False")
-            plateNumber = pytesseract.image_to_string(gray_video)
-            text1 = print(plateNumber)
-            cv2.putText(frame,text=text1,org=(x-3,y-3),fontFace=cv2.FONT_HERSHEY_COMPLEX,color=(0,0,255),thickness=1,fontScale=0.6)
+            cv2.putText(frame,text='License Plate',org=(x-3,y-3),fontFace=cv2.FONT_HERSHEY_COMPLEX,color=(0,0,255),thickness=1,fontScale=0.6)
 
         print(plate)
-         
-    #if ret == True:
         cv2.imshow('Video', frame)
 
         if cv2.waitKey(25) & 0xFF == ord("q"):
